53e44818d38097f8caad85f80abdfb42d187a260
Alessandro Ligugnana <alessandro.ligugnana@gmail.com>
2021-04-22 22:17:43 +0200

    22 Aprile 22:16 Buon punto con la home page dalla parte funzionale, vedere della grafica e della parte interna della pagina del bar, manca tutta la parte degli utenti

---
+++ DatabaseConnection.php
@@ -0,0 +1,19 @@
+<?php
+$mysqli = new mysqli('localhost', 'root', 'root', 'Seats');
+if ($mysqli->connect_error) {
+    die('Errore di connessione (' . $mysqli->connect_errno . ') '
+        . $mysqli->connect_error);
+} else {
+    //echo 'Connesso. ' . $mysqli->host_info . "\n";
+}
+
+
+$sql = "SELECT * FROM Bar";
+$result = mysqli_query($mysqli, $sql);
+
+$mio_array = array();
+while($row = mysqli_fetch_assoc($result)) {
+    $mio_array[] = $row;
+}
+echo json_encode($mio_array);
+
--- script.js
+++ script.js
@@ -1,29 +1,47 @@
-var bars = new Array();
+var arrayBar = new Array();
+var app = angular.module('app', []);
 
-function ricerca() {
-    var input;
-    var filtro;
-    var lista;
-    var voci;
-    var x;
-    var i;
-    var testo;
-    input = document.getElementById("barra-ricerca");
-    filtro = input.value.toUpperCase();
-    lista = document.getElementById("elenco");
-    voci = lista.getElementsByTagName("li");
-    for (i = 0; i < voci.length; i++) {
-        x = lista.getElementsByTagName("li")[i];
-        testo = x.textContent || x.innerText;
-        if (testo.toUpperCase().indexOf(filtro) > -1) {
-            voci[i].style.display = "";
-        } else {
-            voci[i].style.display = "none";
+app.controller('ctrl1', function ($scope){
+    $.get(
+        './DatabaseConnection.php',
+        function( data ) {
+            arrayBar = JSON.parse(data);
+            $scope.$apply(function() {
+                $scope.bars = arrayBar;
+                console.log(arrayBar);
+                $scope.filterCondition = {
+                    nome: 'Name'
+                }
+            });
+        });
+
+    ricerca = function () {
+        var input;
+        var filtro;
+        var lista;
+        var voci;
+        var x;
+        var i;
+        var testo;
+        input = document.getElementById("barra-ricerca");
+        filtro = input.value.toUpperCase();
+        lista = document.getElementById("elenco");
+        voci = lista.getElementsByTagName("li");
+        for (i = 0; i < voci.length; i++) {
+            x = lista.getElementsByTagName("li")[i];
+            testo = x.textContent || x.innerText;
+            if (testo.toUpperCase().indexOf(filtro) > -1) {
+                voci[i].style.display = "";
+            } else {
+                voci[i].style.display = "none";
+            }
         }
     }
-}
 
-function apriPagina(item){
-    location.href = "paginaBar.html"
+     apriPagina = function (item){
+        console.log(item);
+        //location.href = "paginaBar.html"
 
-}
+    }
+
+});
