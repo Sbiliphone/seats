a5718703e87720a1d58978379f984098152bdb01
Alessandro Ligugnana <alessandro.ligugnana@gmail.com>
2021-05-05 22:27:40 +0200

    x

--- templates/homepage.php
+++ templates/homepage.php
@@ -9,7 +9,7 @@ require('../templates/menu.php');
 
 <body ng-app="app">
     <div ng-controller="ctrl1">
-        <div class=".col-md-6 .col-md-offset-3"><input type="text" id="barra-ricerca" onkeyup="ricerca()" placeholder="Cerca"></div>
+        <div class=".col-md-6 .col-md-offset-3"><input type="text" id="barra-ricerca" onkeyup="ricerca()" placeholder="Cerca" class="form-control"></div>
         <div>
             <ul id="elenco" type="none">
                 <li ng-repeat="bar in bars"><div class=".col-md-6">{{bar.Name}}</div><div class=".col-md-6"><button onclick="apriPagina()" class="btn btn-secondary">More Info</button></div><hr></li>
--- templates/menu.php
+++ templates/menu.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light mb-2">
+<nav class="navbar navbar-dark bg-dark">
     <div class="container">
         <a class="navbar-brand" href="index.php">Seats</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent"
