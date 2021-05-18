609d381ebc76e5825e01c5832daf8ad4c0a81b2c
Alessandro Ligugnana <alessandro.ligugnana@gmail.com>
2021-05-19 00:43:13 +0200

    x

--- templates/barInfo.php
+++ templates/barInfo.php
@@ -5,26 +5,88 @@ $bar = $_SESSION['idBar'];
 ?>
 <div style="display: none">
 <?php
+
+function calcolaMedia($nums) {
+    $qt = count($nums);
+    $res = 0;
+    foreach ($nums as $n) {
+        $res += $n;
+    }
+    return $res/$qt;
+}
     global $db;
+    $Monday =[];
+    $Tuesday =[];
+    $Wednesday =[];
+    $Thursday =[];
+    $Friday =[];
+    $Saturday =[];
+    $Sunday =[];
 
     $sql = "SELECT * FROM Bar;";
     $rs = $db->execute($sql);
     global $db;
     $sql="SELECT * FROM Bar WHERE id='$bar';";
     $rs = $db->execute($sql);
+
+    $attualMounth = date("m");
+    $sqlMonday = "SELECT used FROM Report WHERE day='Monday' AND fullDate LIKE '%/$attualMounth/%';";
+    $rsMonday = $db->execute($sqlMonday);
+    foreach ($rsMonday as $risultatoMonday){
+        $Monday = calcolaMedia($risultatoMonday['used']);
+    }
+
+    $sqlTuesday = "SELECT used FROM Report WHERE day='Tuesday' AND fullDate LIKE '%/$attualMounth/%';";
+    $rsTuesday = $db->execute($sqlTuesday);
+    foreach ($rsTuesday as $risultatoTuesday){
+        $Tuesday = calcolaMedia($risultatoTuesday['used']);
+    }
+
+    $sqlWednesday = "SELECT used FROM Report WHERE day='Wednesday' AND fullDate LIKE '%/$attualMounth/%';";
+    $rsWednesday = $db->execute($sqlWednesday);
+    foreach ($rsWednesday as $risultatoWednesday){
+        $Wednesday = calcolaMedia($risultatoWednesday['used']);
+    }
+
+    $sqlThursday = "SELECT used FROM Report WHERE day='Thursday' AND fullDate LIKE '%/$attualMounth/%';";
+    $rsThursday = $db->execute($sqlThursday);
+    foreach ($rsThursday as $risultatoThursday){
+        $Thursday = calcolaMedia($risultatoThursday['used']);
+    }
+
+    $sqlFriday = "SELECT used FROM Report WHERE day='Friday' AND fullDate LIKE '%/$attualMounth/%';";
+    $rsFriday = $db->execute($sqlFriday);
+    foreach ($rsFriday as $risultatoFriday){
+        $Friday = calcolaMedia($risultatoFriday['used']);
+    }
+
+    $sqlSaturday = "SELECT used FROM Report WHERE day='Saturday' AND fullDate LIKE '%/$attualMounth/%';";
+    $rsSaturday = $db->execute($sqlSaturday);
+    foreach ($rsSaturday as $risultatoSaturday){
+        $Saturday = calcolaMedia($risultatoSaturday['used']);
+    }
+
+    $sqlSunday = "SELECT used FROM Report WHERE day='Sunday' AND fullDate LIKE '%/$attualMounth/%';";
+    $rsSunday = $db->execute($sqlSunday);
+    foreach ($rsSunday as $risultatoSunday){
+        $Sunday = calcolaMedia($risultatoSunday['used']);
+    }
 ?>
+
 </div>
+<?php echo $Friday; ?>
 <?php
 foreach ($rs as $risultato){
     $_SESSION['bar']=$risultato['name'];
     //https://www.chartjs.org/docs/latest/
 ?>
-<body>
+<body onload="graphic(<?php echo $Monday; ?>, <?php echo $Tuesday; ?>, <?php echo $Wednesday; ?>, <?php echo $Thursday; ?>, <?php echo $Friday; ?>, <?php echo $Saturday; ?>, <?php echo $Sunday; ?>)">
 <div class="container">
     <br>
     <h1><?php echo $risultato['name']; ?></h1>
     <br>
     <iframe width="500" height="500" src="https://maps.google.com/maps?q=<?php echo $risultato['latitude']; ?>,<?php echo $risultato['longitude']; ?>&output=embed"></iframe>
+    <canvas id="myChart" style="width:100%;max-width:700px"></canvas>
     <?php
     if ($_SESSION['authorized']){
         ?>
@@ -46,8 +108,10 @@ foreach ($rs as $risultato){
     ?>
 
 </div>
-</body>
 
+<script src="../src/scripts/graphic.js"></script>
+
+</body>
 
 
 <?php
